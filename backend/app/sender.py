0ddd9990c9b8e28569c7e2e9448c80b048fa57fc
alex-best <1987lex@gmail.com>
2025-06-24 23:28:41 +0200

    fix: исправлен вызов anyio.run с kwargs для send_to_publisher

--- backend/app/sender.py
+++ backend/app/sender.py
@@ -1,5 +1,5 @@
-import httpx
 import anyio
+import httpx
 
 publisher_url: str = "http://10.0.15.4:8012/publish"
 
@@ -26,6 +26,10 @@ async def send_to_publisher_async(
         response.raise_for_status()
         return response.json()["message_id"]
 
-def send_to_publisher(*args, **kwargs):
+# def send_to_publisher(*args, **kwargs):
+def send_to_publisher(token, chat_id, parse_mode, text=None, caption=None, image_path=None):
     # Синхронная обёртка для вызова из Celery (используется anyio)
-    return anyio.run(send_to_publisher_async, *args, **kwargs)
+    return anyio.run(
+        send_to_publisher_async,
+        token, chat_id, parse_mode, text, caption, image_path
+    )
