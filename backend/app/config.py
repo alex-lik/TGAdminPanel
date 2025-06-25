848d33309e4be4b8300178c06d7a933faedff1ce
alex-best <1987lex@gmail.com>
2025-06-25 11:53:05 +0200

    проверка наличия переменной окружения перед загрузкой .env

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,9 +2,12 @@ from os import getenv
 
 from dotenv import load_dotenv
 
-load_dotenv()
+if not getenv("SQLALCHEMY_DATABASE_URL"):
+    load_dotenv()
+
 
 SQLALCHEMY_DATABASE_URL = getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./telegram_content.db")
 
 LOG_PATH = './logs/today.log'
+
 
